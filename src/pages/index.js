88cd0179f2ca2db081f72d076088e63d5b24223f
Vojta Oupicky <voj.oup@gmail.com>
2018-12-29 20:43:17 +0100

    Get rid of forms

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,26 +28,6 @@ const IndexPage = () => (
         }
       }) => edges.map(({ node }, i) => <RecipeItem {...node} key={i} />)}
     />
-    <form action="/success" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Your Role: <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
   </Layout>
 );
 
